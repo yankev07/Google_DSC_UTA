12aa3d624bb3c796d8807ee59c2eea076d2e03c3
Raja Krishna <rajakrishna214@gmail.com>
2019-09-19 19:25:00 -0500

    update

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-(function() {
-    window.onresize = displayWindowSize;
-    window.onload = displayWindowSize;
+  var vsOpts = {
+    $slides: $('.v-slide'),
+    $list: $('.v-slides'),
+    duration: 6,
+    lineHeight: 50
+  }
   
-    function displayWindowSize() {
-      myWidth = window.innerWidth;
-      myHeight = window.innerHeight;
-      // your size calculation code here
-      document.getElementById("screen").innerHTML = myWidth + "x" + myHeight;
-    };
+  var vSlide = new TimelineMax({
+    paused: true,
+    repeat: -1
+    // yoyo: true
+  })
   
-  
-  })();
+  vsOpts.$slides.each(function(i) {
+    vSlide.to(vsOpts.$list, vsOpts.duration / vsOpts.$slides.length, {
+      y: i * -1 * vsOpts.lineHeight,
+      // ease:Elastic.easeOut.config(1,0.8)
+      ease: Power4.easeOut
+    })
+  })
+  vSlide.play()
