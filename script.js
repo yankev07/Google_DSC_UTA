736fbf855c9e7745a9ae73b6cc1929d0e8cc6b0d
Raja Krishna <rajakrishna214@gmail.com>
2019-10-10 14:25:14 -0500

    simple-changes

--- script.js
+++ script.js
@@ -3,21 +3,21 @@
     $list: $('.v-slides'),
     duration: 6,
     lineHeight: 50
-  }
+  };
   
   var vSlide = new TimelineMax({
     paused: true,
     repeat: -1
-  })
+  });
   
   vsOpts.$slides.each(function(i) {
     vSlide.to(vsOpts.$list, vsOpts.duration / vsOpts.$slides.length, {
       y: i * -1 * vsOpts.lineHeight,
       // ease:Elastic.easeOut.config(1,0.8)
       ease: Power4.easeOut
-    })
-  })
-  vSlide.play()
+    });
+  });
+  vSlide.play();
 
 
   function formSubmit() {
